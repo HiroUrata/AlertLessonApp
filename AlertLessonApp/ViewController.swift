a9a60fb4f4003b6edfa78478125de27fbfffc85e
HiroUrata <1712026@i.softbank.jp>
2021-09-15 21:56:32 +0900

    usualAlert(_ sender: Any) 処理追加

--- AlertLessonApp/ViewController.swift
+++ AlertLessonApp/ViewController.swift
@@ -14,6 +14,46 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func usualAlert(_ sender: Any) {
+        
+        let usualAler = {() -> UIAlertController in
+            
+            let alert = UIAlertController(title: "タイトルだよ", message: "アラートの内容だよ", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { _ in
+                
+                print("Yes")
+                
+            }))
+            
+            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { _ in
+                
+                print("No")
+                
+            }))
+            
+            return alert
+        }()
+        
+        self.present(usualAler, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func usualActionSheetAlert(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func textfieldInAlert(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func textfieldInActionSheetAlert(_ sender: Any) {
+        
+        
+    }
+    
+    
 }
 
