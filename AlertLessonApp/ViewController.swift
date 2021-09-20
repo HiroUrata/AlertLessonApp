835487662d80bf0dfda19bee74d0b227ab89ec5e
HiroUrata <1712026@i.softbank.jp>
2021-09-20 22:59:33 +0900

    navigationItem.searchController 追加

--- AlertLessonApp/ViewController.swift
+++ AlertLessonApp/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationItem.searchController = UISearchController()
+        
     }
 
     @IBAction func usualAlert(_ sender: Any) {
