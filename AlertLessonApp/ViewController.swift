858a74b38a2eee272b6737f1643ee3fe79674271
HiroUrata <1712026@i.softbank.jp>
2021-09-20 23:31:20 +0900

    searchController　コード変更　title　追加

--- AlertLessonApp/ViewController.swift
+++ AlertLessonApp/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        navigationItem.searchController = UISearchController()
+        let searchbar = UISearchController()
+        
+        navigationItem.searchController = searchbar
+        title = "AlertLesson"
         
     }
 
